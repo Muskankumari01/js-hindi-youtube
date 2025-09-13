a4c98283acf9e9eac24d27e6ccdf5e9c95530f5e
Muskan kumari <125817964+Muskankumari01@users.noreply.github.com>
2025-09-13 19:57:50 +0000

    Datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all JS code as newer version
+
+alert(3 + 3 ) // we are using nodejs, not browser
+
+console.log (3
+    +
+    3) //code readability should be high
+    
+console.log("Muskan")
+
+
+let name = "Muskan" // string datatype 
+let age = 18  // number datatype
+let isLoggedIn = false // boolean datatype
+let state;
+
+
+// this down all is (primitive datatype upto symbol)
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true or false
+// null => standalone value
+// undefined => no value defined or assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined (only output come)
+console.log(typeof null); // object (for this output will come object only)
