abb384fda2baf787a52a13c0ebcddf7f486dad6a
Muskan kumari <125817964+Muskankumari01@users.noreply.github.com>
2025-09-13 20:46:01 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "muskan"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+ 
+let valueInnumber = Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+//" " => string
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Muskan" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
